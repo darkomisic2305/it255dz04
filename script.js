9319b4f3cf16407d9f146cdac4fc1b0000a7aa55
darkomisic2305 <darko.misic.2305@metropolitan.ac.rs>
2016-05-07 13:23:12 +0200

    IT255DZ04 commit

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Proba da li radi JavaScript!");
+function funkcija(id) {
+	document.getElementById(id).style.border = "5px dotted gray";
+}
+function funkcija2(id) {
+	document.getElementById(id).style.border = "0px";
+}
